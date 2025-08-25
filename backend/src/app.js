9d37c75085ab4187e14bc212b0eef84cd1639c11
ShubHH03 <mak.shubham2003@gmail.com>
2025-08-25 21:15:16 +0530

    feat: add healthcheck endpoint for server status verification

--- backend/src/app.js
+++ backend/src/app.js
@@ -9,6 +9,12 @@ app.use(express.json())
 //routes import
 import router from "./routes/url.route.js"
 
+// For Render
+app.get("/healthcheck", (req, res) => {
+  res.status(200).send("OK");
+});
+
+
 //routes declaration
 app.use("/api", router)
 
